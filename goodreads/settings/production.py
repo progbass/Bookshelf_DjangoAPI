5ad80fc0ac878de2865a469b42bfbbaf8e630db4
progbass <progbass@gmail.com>
2017-09-29 20:46:36 -0500

    Updated prosuctions settings

--- goodreads/settings/production.py
+++ goodreads/settings/production.py
@@ -3,18 +3,14 @@ import os
 import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oji_onyq4-^h10+ho9%7r7mf-6fv7q+!wrzp65co8)5w^)!!dh'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY =  os.getenv('SECRET_KEY')
 DEBUG = False
-
-# Allowed Hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Database
 DATABASES = dict()
 DATABASES['default'] = dj_database_url.config() # <--- Heroku Only Configuration
 SESSION_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https://')
 STATIC_ROOT = os.path.join(os.getcwd(), 'static')
-
+afternoon-woodland-60928
 

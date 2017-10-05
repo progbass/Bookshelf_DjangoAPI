904dfb7dcdd4c2e0c7b94e030be4f6021dc0defe
progbass <progbass@gmail.com>
2017-10-04 23:31:40 -0500

    Added Homepage

--- goodreads/wsgi.py
+++ goodreads/wsgi.py
@@ -6,11 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
-import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise	
 from dotenv import load_dotenv
+#from whitenoise.django import DjangoWhiteNoise	
 
 try:
     dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
